c6f5ff9ecc67dc36eb8c806027e4c3aae8b5012f
ara <arw001@edu.zealand.dk>
2026-05-25 18:07:22 +0200

    5/25- 2026 18:06 about to start js coding?

--- javascrip.js
+++ javascrip.js
@@ -4,3 +4,24 @@
     burger.addEventListener("click", () => {
         nav.classList.toggle("active");
     });
+
+    //quick scroll
+
+    const scrollBtn = document.getElementById("scrollTopBtn");
+
+    // show button when scrolling
+    window.onscroll = function () {
+        if (document.documentElement.scrollTop > 200) {
+            scrollBtn.style.display = "block";
+        } else {
+            scrollBtn.style.display = "none";
+        }
+    };
+
+    // scroll to top on click
+    scrollBtn.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
